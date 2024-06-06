0d237149f659ed9b2e1cf2f6cdecc8e1a7f16e3d
hunterfaugot <hbf9768@gmail.com>
2024-06-05 21:09:01 -0500

    changed ' to apos

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="text-center py-20 transition-all duration-300">
       <h1 className="text-5xl font-bold mb-4">Welcome to My Portfolio</h1>
-      <p className="text-2xl mb-8">Hello, I'm Hunter Faugot.</p>
+      <p className="text-2xl mb-8">Hello, I&apos;m Hunter Faugot.</p>
     </div>
   );
 }
